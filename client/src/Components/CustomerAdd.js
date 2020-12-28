e08be17bdde9e98a19cd4783b5eeb1e7ffd54f9a
leejiwon6315 <leejiwon6315@naver.com>
2020-12-28 15:36:08 +0900

    add refresh module

--- client/src/Components/CustomerAdd.js
+++ client/src/Components/CustomerAdd.js
@@ -18,6 +18,7 @@ class CustomerAdd extends React.Component {
     e.preventDefault();
     this.addCustomer().then((response) => {
       console.log(response.data);
+      this.props.stateRefresh();
     });
 
     this.setState({
