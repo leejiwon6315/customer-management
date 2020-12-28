db9686276d5b5176418f49f3051330b14e4d2e49
leejiwon6315 <leejiwon6315@naver.com>
2020-12-28 11:29:45 +0900

    add CustomerAdd form

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
+import React, { Component } from "react";
 import Customer from "./Components/Customer";
+import CustomerAdd from "./Components/CustomerAdd";
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
@@ -8,7 +10,6 @@ import TableCell from "@material-ui/core/TableCell";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 import { withStyles } from "@material-ui/core/styles";
-import { Component } from "react";
 
 const styles = (theme) => ({
   root: {
@@ -54,48 +55,52 @@ class App extends Component {
     const { classes } = this.props;
 
     return (
-      <Paper className={classes.root}>
-        <h1>고객정보</h1>
-        <Table clsssName={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell> 번호 </TableCell>
-              <TableCell> 사진 </TableCell>
-              <TableCell> 이름 </TableCell>
-              <TableCell> 생일 </TableCell>
-              <TableCell> 성별 </TableCell>
-              <TableCell> 직업 </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? (
-              this.state.customers.map((c) => {
-                return (
-                  <Customer
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                );
-              })
-            ) : (
+      <>
+        <Paper className={classes.root}>
+          <h1>고객정보</h1>
+          <Table clsssName={classes.table}>
+            <TableHead>
               <TableRow>
-                <TableCell colspan="6" align="center">
-                  <CircularProgress
-                    className={classes.progress}
-                    variant="determinate"
-                    value={this.state.completed}
-                  />
-                </TableCell>
+                <TableCell> 번호 </TableCell>
+                <TableCell> 사진 </TableCell>
+                <TableCell> 이름 </TableCell>
+                <TableCell> 생일 </TableCell>
+                <TableCell> 성별 </TableCell>
+                <TableCell> 직업 </TableCell>
               </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </Paper>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? (
+                this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              ) : (
+                <TableRow>
+                  <TableCell colspan="6" align="center">
+                    <CircularProgress
+                      className={classes.progress}
+                      variant="determinate"
+                      value={this.state.completed}
+                    />
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </Paper>
+
+        <CustomerAdd />
+      </>
     );
   }
 }
--- client/src/Components/CustomerAdd.js
+++ client/src/Components/CustomerAdd.js
@@ -14,6 +14,45 @@ class CustomerAdd extends React.Component {
     };
   }
 
+  addCustomer = () => {
+    const url = "/api/customers";
+    const formData = new FormData();
+
+    formData.append("image", this.state.file);
+    formData.append("name", this.state.userName);
+    formData.append("birthday", this.state.birthday);
+    formData.append("gender", this.state.gender);
+    formData.append("job", this.state.job);
+
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+
+    return post(url, formData, config);
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.addCustomer().then((response) => {
+      console.log(response.data);
+    });
+  };
+
+  handleFileChange = (e) => {
+    this.setState({
+      file: e.target.files[0],
+      fileName: e.target.value,
+    });
+  };
+
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  };
+
   render() {
     return (
       <form onSubmit={this.handleFormSubmit}>
