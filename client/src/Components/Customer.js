265533793fd8bf654d7f23956d844f7b9d66e35d
leejiwon6315 <leejiwon6315@naver.com>
2020-12-28 16:43:16 +0900

    add customerDelete

--- client/src/Components/Customer.js
+++ client/src/Components/Customer.js
@@ -1,20 +1,29 @@
 import React from "react";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CustomerDelete from "./CustomerDelete";
 
-const Customer = ({ image, name, id, birthday, gender, job }) => {
-  return (
-    <TableRow>
-      <TableCell>{id}</TableCell>
-      <TableCell>
-        <img src={image} alt="profile" />
-      </TableCell>
-      <TableCell>{name}</TableCell>
-      <TableCell>{birthday}</TableCell>
-      <TableCell>{gender}</TableCell>
-      <TableCell>{job}</TableCell>
-    </TableRow>
-  );
-};
+class Customer extends React.Component {
+  render() {
+    return (
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img src={this.props.image} alt="profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+        <TableCell>
+          <CustomerDelete
+            stateRefresh={this.props.stateRefresh}
+            id={this.props.id}
+          />
+        </TableCell>
+      </TableRow>
+    );
+  }
+}
 
 export default Customer;
