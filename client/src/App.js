4967ebaa8b5d307623fed7467d7f7d028a229c7b
leejiwon6315 <leejiwon6315@naver.com>
2020-12-29 13:39:43 +0900

    add menu bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Customer from "./Components/Customer";
 import CustomerAdd from "./Components/CustomerAdd";
+
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
@@ -8,18 +9,92 @@ import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import CircularProgress from "@material-ui/core/CircularProgress";
-
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { fade } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = (theme) => ({
   root: {
-    width: "1700px",
-    marginTop: theme.spacing(3),
-    overFlowX: "auto",
+    width: "100vw",
+  },
+
+  appBar: {
+    backgroundColor: "rgb(45, 56, 78)",
+  },
+
+  menuButton: {
+    marginRight: theme.spacing(2),
+    color: "#FE9677",
+  },
+
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  inputRoot: {
+    color: "inherit",
+  },
+
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+
+  tableWrap: {
+    margin: 20,
   },
 
   table: {
     minWidth: 1080,
+  },
+
+  tableHead: {
+    fontSize: 15,
+    fontWeight: 400,
   },
 
   progress: {
@@ -67,21 +142,55 @@ class App extends Component {
 
   render() {
     const { classes } = this.props;
-
+    const cellList = [
+      "번호",
+      "사진",
+      "이름",
+      "생년월일",
+      "성별",
+      "직업",
+      "설정",
+    ];
     return (
-      <>
-        <Paper className={classes.root}>
-          <h1>고객정보</h1>
+      <div className={classes.root}>
+        <AppBar position="static" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              고객 관리 시스템
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="검색"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+
+        <Paper className={classes.tableWrap}>
           <Table clsssName={classes.table}>
             <TableHead>
               <TableRow>
-                <TableCell> 번호 </TableCell>
-                <TableCell> 사진 </TableCell>
-                <TableCell> 이름 </TableCell>
-                <TableCell> 생일 </TableCell>
-                <TableCell> 성별 </TableCell>
-                <TableCell> 직업 </TableCell>
-                <TableCell> 설정 </TableCell>
+                {cellList.map((c) => {
+                  return (
+                    <TableCell className={classes.tableHead}>{c}</TableCell>
+                  );
+                })}
               </TableRow>
             </TableHead>
 
@@ -117,7 +226,7 @@ class App extends Component {
         </Paper>
 
         <CustomerAdd stateRefresh={this.stateRefresh} />
-      </>
+      </div>
     );
   }
 }
